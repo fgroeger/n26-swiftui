fd0d567fdec325bdf4f44244180803f8a5820a6a
Fabian Gröger <fabian.groeger@icloud.com>
2021-01-21 23:29:04 +0100

    Initial commit

---
+++ N26Clone/N26Clone/N26CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  N26CloneApp.swift
+//  N26Clone
+//
+//  Created by Fabian Gröger on 21.01.21.
+//
+
+import SwiftUI
+
+@main
+struct N26CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
